abc9b89ece4ca14c135a90b181869c682c67edf0
Jay Bazuzi <JAY@BAZUZI.COM>
2017-03-18 12:47:01 -0700

    make build.cmd with spaces in the path

--- build.cmd
+++ build.cmd
@@ -1,6 +1,6 @@
 @SETLOCAL
 @IF NOT EXIST .build mkdir .build
 cd .build
-cmake -Werror=dev -Werror=deprecated %~dp0
+cmake -Werror=dev -Werror=deprecated "%~dp0"
 cmake --build . -- /verbosity:minimal
 ctest
