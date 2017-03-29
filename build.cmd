2f49da999ceb3e26a6cbd6c0c49eb2902e97abb9
Jay Bazuzi <jay@bazuzi.com>
2017-03-28 17:34:25 -0700

    exec routine, turn off echo

--- build.cmd
+++ build.cmd
@@ -1,6 +1,12 @@
 @SETLOCAL
+@ECHO OFF
 @IF NOT EXIST .build mkdir .build
-cd .build
-cmake -Werror=dev -Werror=deprecated "%~dp0"
-cmake --build . -- /verbosity:minimal
-ctest
+call :exec cd .build
+call :exec cmake -Werror=dev -Werror=deprecated "%~dp0"
+call :exec cmake --build . -- /verbosity:minimal
+call :exec ctest
+goto :EOF
+
+    :exec
+echo %~nx0: %*
+%*
