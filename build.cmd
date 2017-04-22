afc395f17e150d7e9c3cd1c92cf6385f332e8929
Jay Bazuzi <JAY@BAZUZI.COM>
2017-04-22 15:59:02 -0700

    verbose ctest output

--- build.cmd
+++ build.cmd
@@ -4,7 +4,7 @@
 call :exec cd .build
 call :exec cmake -Werror=dev -Werror=deprecated "%~dp0"
 call :exec cmake --build . -- /verbosity:minimal
-call :exec ctest
+call :exec ctest --verbose
 goto :EOF
 
     :exec
