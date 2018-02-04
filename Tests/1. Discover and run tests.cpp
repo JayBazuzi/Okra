850cb516371231760268a52727a36fd415cfcaa7
Jay Bazuzi <jbazuzi@tableau.com>
2018-02-04 08:00:22 -0800

    R: rename parameter

--- Tests/1. Discover and run tests.cpp
+++ Tests/1. Discover and run tests.cpp
@@ -42,7 +42,7 @@ public:
 	using duration = std::chrono::duration<uint64_t, std::ratio<1, 1000000>>;
 	using time_point = std::chrono::time_point<ArtificialClock>;
 
-	static void advance(duration d) noexcept { nowMicroseconds += d; }
+	static void advance(duration duration) noexcept { nowMicroseconds += duration; }
 	static time_point now() noexcept { return nowMicroseconds; }
 
 private:
