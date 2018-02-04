f93c6f52606eefd917c5ff2156e489ca78d4886e
Jay Bazuzi <JAY@BAZUZI.COM>
2018-02-04 13:15:52 -0800

    quieter, more stringent build options

--- Okra.h
+++ Okra.h
@@ -1,6 +1,5 @@
 #pragma once
 
-#include <algorithm>
 #include <chrono>
 #include <functional>
 #include <iostream>
--- build-and-test.cmd
+++ build-and-test.cmd
@@ -4,10 +4,10 @@
 @IF NOT EXIST .build mkdir .build
 cd .build
 
-call :exec cmake ..
+call :exec cmake .. -Wdev -Werror=dev -Wdeprecated -Werror=deprecated -G "Visual Studio 15 2017"
 if ERRORLEVEL 1 goto :EOF
 
-call :exec cmake --build .
+call :exec cmake --build . -- /verbosity:minimal /nologo
 if ERRORLEVEL 1 goto :EOF
 
 set CTEST_OUTPUT_ON_FAILURE=1
@@ -17,5 +17,6 @@ if ERRORLEVEL 1 goto :EOF
 goto :EOF
 
     :exec
+echo.
 echo %~nx0: %*
 %*
