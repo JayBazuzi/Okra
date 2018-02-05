cb6b8375b9181ec4a317ef1fe1e2cbab9a67725c
Jay Bazuzi <JAY@BAZUZI.COM>
2018-02-04 19:59:27 -0800

    a couple more assertion tests

--- Tests/4. Assertions.cpp
+++ Tests/4. Assertions.cpp
@@ -22,6 +22,24 @@ TEST("FAIL")
 
 	const auto string_listener = std::make_shared<StringListener>();
 	ASSERT(!subject.Run({string_listener}));
-
+	ASSERT_EQUAL(1, string_listener->fails.size());
 	ASSERT_EQUAL(":  - assert FAILED - \nblah\n", string_listener->fails[0]);
+}
+
+TEST("ASSERT fail")
+{
+	const okra::TestInfo subject{"", []() { ASSERT(true); }};
+
+	const auto string_listener = std::make_shared<StringListener>();
+	ASSERT(subject.Run({string_listener}));
+	ASSERT_EQUAL(0, string_listener->fails.size());
+}
+
+TEST("ASSERT fail")
+{
+	const okra::TestInfo subject{"", []() { ASSERT(false); }};
+	const auto string_listener = std::make_shared<StringListener>();
+	ASSERT(!subject.Run({string_listener}));
+	ASSERT_EQUAL(1, string_listener->fails.size());
+	ASSERT_EQUAL(":  - assert FAILED - \nfalse\n", string_listener->fails[0]);
 }
