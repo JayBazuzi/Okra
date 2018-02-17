006111157f3cce014eb86a073b67140fa51fd2ea
Jay Bazuzi <JAY@BAZUZI.COM>
2018-02-17 13:30:23 -0800

    report more to the console first

--- Tests/6. Approvals.cpp
+++ Tests/6. Approvals.cpp
@@ -1,4 +1,17 @@
 #define APPROVALS_OKRA
 #include "ApprovalTests.v.1.0.0.hpp"
+using namespace std;
 
-TEST("TestNameMayNotContainsSpaces") { Approvals::verify("foo"); }
+class ConsoleDiffReporter : public Reporter
+{
+	bool Report(std::string received, std::string approved) const override
+	{
+		cerr << received << " exists " << FileUtils::fileExists(received) << endl;
+		cerr << approved << " exists " << FileUtils::fileExists(approved) << endl;
+		cerr << received << " size " << FileUtils::fileSize(received) << endl;
+		cerr << approved << " size " << FileUtils::fileSize(approved) << endl;
+		return DiffReporter().Report(received, approved);
+	}
+};
+
+TEST("TestNameMayNotContainsSpaces") { Approvals::verify("foo", ConsoleDiffReporter()); }
