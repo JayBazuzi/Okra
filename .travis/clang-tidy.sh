d47fd70060e49e28611f501ce1c0497e277ae8b6
Jay Bazuzi <jbazuzi@tableau.com>
2018-09-03 17:21:29 -0700

    tidy, not format

--- .travis/clang-tidy.sh
+++ .travis/clang-tidy.sh
@@ -3,9 +3,9 @@
 result=0
 find . -name '*.cpp' -or -name '*.h' | while read file
 do
-    d=$(diff -u "$file" <(clang-format "$file") )
+    d=$(diff -u "$file" <(clang-tidy "$file") )
     if ! [ -z "$d" ]; then
-        echo "!!! $file not clang-format compliant"
+        echo "!!! $file not clang-tidy compliant"
         result=1
     fi
 done
