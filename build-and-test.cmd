611f036d32ae95f32f16d282534e96f4abfa32fb
Jay Bazuzi <jbazuzi@tableau.com>
2019-09-27 17:19:50 -0700

    batch syntax trivia

--- build-and-test.cmd
+++ build-and-test.cmd
@@ -1,20 +1,20 @@
 @SETLOCAL
 @ECHO OFF
 
-@IF NOT EXIST .build mkdir .build
+IF NOT EXIST .build mkdir .build
 cd .build
 
-call :exec cmake .. -Wdev -Werror=dev -Wdeprecated -Werror=deprecated -G "Visual Studio 15 2017"
-if ERRORLEVEL 1 goto :EOF
+CALL :exec cmake .. -Wdev -Werror=dev -Wdeprecated -Werror=deprecated -G "Visual Studio 15 2017"
+IF ERRORLEVEL 1 GOTO :EOF
 
-call :exec cmake --build . -- /verbosity:minimal /nologo
-if ERRORLEVEL 1 goto :EOF
+CALL :exec cmake --build . -- /verbosity:minimal /nologo
+IF ERRORLEVEL 1 GOTO :EOF
 
 set CTEST_OUTPUT_ON_FAILURE=1
-call :exec ctest --parallel 4
-if ERRORLEVEL 1 goto :EOF
+CALL :exec ctest --parallel 4
+IF ERRORLEVEL 1 GOTO :EOF
 
-goto :EOF
+GOTO :EOF
 
     :exec
 echo.
